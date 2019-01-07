2b47925a70f6795677d3927b63bbc8298b92e975
phatblinkie <phatblinkie@hotmail.com>
2019-01-07 16:03:30 +0000

    removed pool

--- stats.php
+++ stats.php
@@ -2,8 +2,8 @@
 <tr>
   <td class="text-center"><img height="40px" src="/images/pirl_logo.png" /></td>
   <td>Pirl</td>
-  <td>11.99GH</td>
-  <td>58</td>
+  <td>12.01GH</td>
+  <td>57</td>
   <td>8</td>
   <td>0.031991</td>
   <td>0.00000845</td>
@@ -14,9 +14,9 @@
 <tr>
   <td class="text-center"><img height="40px" src="/images/akroma_logo.png" /></td>
   <td>Akroma</td>
-  <td>742.89MH</td>
-  <td>8</td>
-  <td>2</td>
+  <td>834.89MH</td>
+  <td>9</td>
+  <td>0</td>
   <td>0.014342</td>
   <td>0.00000378</td>
   <td>-10.700</td>
@@ -29,8 +29,8 @@
   <td>0.00H</td>
   <td>0</td>
   <td>0</td>
-  <td>0.572214</td>
-  <td>0.00014091</td>
-  <td>2.040</td>
+  <td>0.570963</td>
+  <td>0.00014036</td>
+  <td>3.860</td>
   <td class="text-center"><a href="https://MOAC.cryptopools.info" class="btn btn-success">Mine Now</a></td>
 </tr>
